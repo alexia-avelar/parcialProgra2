9eeee1203d8dc9718970c3e5679e1d299aaf2df6
alexia-avelar <72109860+alexia-avelar@users.noreply.github.com>
2021-02-23 21:30:26 -0600

    Add files via upload

--- pregunta3/script.js
+++ pregunta3/script.js
@@ -1,70 +1,64 @@
 var total = 0;
 var diferencia = 0;
+var subtotal = 0;
 var presupuesto = 0;
 
 function sumaTotal() {
     diferencia = total - presupuesto;
-    document.getElementById("totalText").innerHTML = total + ".";
-    document.getElementById("diferenciaText").innerHTML = diferencia + ".";
+    document.getElementById("total").innerHTML = total + ".";
+    document.getElementById("diferencia").innerHTML = diferencia + ".";
 }
 
 function addRow() {
     producto = document.getElementById("producto").value;
     precio = document.getElementById("precio").value;
     cantidad = document.getElementById("cantidad").value;
-    presupuesto = document.getElementById("presupuesto").value;
 
-    if (producto == "" && precio == "" && cantidad == "" && presupuesto == "") {
-        alert("Faltan campos por llenar");
+    celda1 = document.createElement("td");
+    celda1.innerHTML = [producto];
+
+    celda2 = document.createElement("td");
+    celda2.innerHTML = [precio];
+
+    celda3 = document.createElement("td");
+    celda3.innerHTML = [cantidad];
+
+    celda4 = document.createElement("td");
+    subsubtotal = precio * cantidad;
+    subtotal += subsubtotal;
+    celda4.innerHTML = [subsubtotal];
+    celda4.className = "subtotal";
+
+    if (subsubtotal > presupuesto) {
+        celda5 = document.createElement("td");
+        celda5.innerHTML = ["sobre"];
     }
     else {
-
-        table = document.getElementById("table");
-        next = document.getElementById("next");
-
-        body = document.createElement("thead");
-        row = document.createElement("tr");
-
-        cell1 = document.createElement("td");
-        cell1.innerHTML = [producto];
-
-        cell2 = document.createElement("td");
-        cell2.innerHTML = [precio];
-
-        cell3 = document.createElement("td");
-        cell3.innerHTML = [cantidad];
-
-        cell4 = document.createElement("td");
-        subtotal = cantidad * precio;
-        total += subtotal;
-        cell4.innerHTML = [subtotal];
-        cell4.className = "sub";
-
-        if (subtotal > presupuesto) {
-            cell5 = document.createElement("td");
-            cell5.innerHTML = ["Sobre"];
-        }
-        else {
-            cell5 = document.createElement("td");
-            cell5.innerHTML = ["Dentro"];
-        }
-
-        row.appendChild(cell1);
-        row.appendChild(cell2);
-        row.appendChild(cell3);
-        row.appendChild(cell4);
-        row.appendChild(cell5);
-
-
-        body.appendChild(row);
-        table.appendChild(body);
-
-        console.log(row);
-        console.log(compra, precio, cantidad, presupuesto);
-
-        document.getElementById("form").reset();
-
+        celda5 = document.createElement("td");
+        celda5.innerHTML = ["dentro"];
     }
+
+    tabla = document.getElementById("tabla");
+    next = document.getElementById("next");
+
+    body = document.createElement("thead");
+    row = document.createElement("tr");
+
+    row.appendChild(celda1);
+    row.appendChild(celda2);
+    row.appendChild(celda3);
+    row.appendChild(celda4);
+    row.appendChild(celda5);
+
+    body.appendChild(row);
+    tabla.appendChild(body);
 }
+
+console.log(row);
+console.log(compra, precio, cantidad, presupuesto);
+
+document.getElementById("form").reset();
+
+
 
 
